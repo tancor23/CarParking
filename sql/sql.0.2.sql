c0277abd5e8e650eca6c83fe928cf8932adc836b
Vydra Sergei <35927520+tancor23@users.noreply.github.com>
2018-04-18 13:55:05 +0300

    Add files via upload
    add new info

---
+++ sql/sql.0.2.sql
@@ -0,0 +1,86 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.17-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for carparking
+CREATE DATABASE IF NOT EXISTS `carparking` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `carparking`;
+
+-- Dumping structure for table carparking.cars
+CREATE TABLE IF NOT EXISTS `cars` (
+  `id_car` int(11) NOT NULL AUTO_INCREMENT,
+  `brand` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_car`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table carparking.cars: ~6 rows (approximately)
+DELETE FROM `cars`;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` (`id_car`, `brand`, `model`) VALUES
+	(1, 'ford', 'focus'),
+	(2, 'ford', 'ka'),
+	(3, 'ford', 'mondeo'),
+	(4, 'ford', 'kuga'),
+	(5, 'opel', 'omega'),
+	(7, '3', '3'),
+	(8, '123', '123');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+
+-- Dumping structure for table carparking.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id_order` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `id_car` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_order`),
+  KEY `FK_orders_cars` (`id_car`),
+  KEY `FK_orders_users` (`id_user`),
+  CONSTRAINT `FK_orders_cars` FOREIGN KEY (`id_car`) REFERENCES `cars` (`id_car`),
+  CONSTRAINT `FK_orders_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table carparking.orders: ~0 rows (approximately)
+DELETE FROM `orders`;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` (`id_order`, `id_user`, `id_car`) VALUES
+	(1, 1, 2),
+	(2, 2, 7),
+	(3, 4, 3),
+	(4, 3, 5),
+	(5, 6, 1),
+	(6, 5, 4);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+
+-- Dumping structure for table carparking.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table carparking.users: ~3 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id_user`, `name`, `email`) VALUES
+	(1, 'Sergei', 'tancor23@mail.ru'),
+	(2, 'Anton', '123@mail.ru'),
+	(3, 'Reggi', '1@mail.ru'),
+	(4, 'Serg', 'Ser@mail.ru'),
+	(5, 'Ress', 'ress@mail.ru'),
+	(6, 'Tasha', 'tasha@mail.ru');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
